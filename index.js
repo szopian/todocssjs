15f1e4e9f063a9cbf4c04a1d67cf11d7c8c9313b
szopian <szopiangreg@gmail.com>
2022-02-04 15:35:18 +0000

    add changes

--- index.js
+++ index.js
@@ -30,18 +30,20 @@ function main() {
       const cards = [...this.querySelectorAll(".card")];
       const currPos = cards.indexOf(draggingCard);
       const newPos = cards.indexOf(e.target);
-      console.log(currPos, newPos);
+      //console.log(currPos, newPos);
       if (currPos > newPos) {
         this.insertBefore(draggingCard, e.target);
       } else {
         this.insertBefore(draggingCard, e.target.nextSibling);
       }
+
       const todos = JSON.parse(localStorage.getItem("todos"));
       const removed = todos.splice(currPos, 1);
       todos.splice(newPos, 0, removed[0]);
       localStorage.setItem("todos", JSON.stringify(todos));
     }
   });
+
   // add new todos on user input
   const add = document.getElementById("add-btn");
   const txtInput = document.querySelector(".txt-input");
